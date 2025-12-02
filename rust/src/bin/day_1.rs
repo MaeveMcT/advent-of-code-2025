0871ea7aa54ebf3f1a69d2752d804cac20cec828
MaeveMcT <rmctiernan@protonmail.com>
2025-12-02 00:11:49 +0000

    day 1 part 2
    Why was that so difficult

--- rust/src/bin/day_1.rs
+++ rust/src/bin/day_1.rs
@@ -11,6 +11,13 @@ fn main() {
 
     let turns: Vec<&str> = input.lines().collect();
 
+    //let turns = vec!["R50"]; // Should be 1 click
+    //let turns = vec!["R51"]; // Should be 1 click
+    //let turns = vec!["L50", "L100"]; // Should be 2 click
+    //let turns = vec!["L50", "L200"]; // Should be 3 click
+    //let turns = vec!["L50", "L5"];
+    //let turns = vec!["R1000"]; // Should be 10 clicks
+
     let mut times_hit_0 = 0;
     let mut current_combination = 50;
     for turn in turns {
@@ -18,22 +25,30 @@ fn main() {
         let rotation_amount: i32 = rotation_amount.parse().unwrap();
 
         match direction {
-            "R" => current_combination += rotation_amount,
-            "L" => current_combination -= rotation_amount,
+            "R" => {
+                current_combination += rotation_amount;
+                times_hit_0 += current_combination / 100;
+                current_combination = current_combination.rem_euclid(100);
+            }
+            "L" => {
+                let old_combination = current_combination;
+                current_combination -= rotation_amount;
+                current_combination = current_combination.rem_euclid(100);
+                if old_combination == 0 {
+                    times_hit_0 += rotation_amount / 100;
+                } else if rotation_amount > old_combination {
+                    times_hit_0 += 1;
+                    times_hit_0 += (rotation_amount - old_combination - 1) / 100;
+                    if current_combination == 0 {
+                        times_hit_0 += 1;
+                    }
+                } else if rotation_amount == old_combination {
+                    times_hit_0 += 1;
+                }
+            }
             _ => {
                 unreachable!()
             }
-        }
-
-        while current_combination < 0 {
-            current_combination += 100
-        }
-        while current_combination >= 100 {
-            current_combination -= 100
-        }
-
-        if current_combination == 0 {
-            times_hit_0 += 1;
         }
     }
 
