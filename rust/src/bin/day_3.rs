eb7ed74810b6424bd1b6628f6bcda5b29f202864
MaeveMcT <rmctiernan@protonmail.com>
2025-12-05 00:56:07 +0000

    tests god help me day 3 part 2

--- rust/src/bin/day_3.rs
+++ rust/src/bin/day_3.rs
@@ -12,55 +12,7 @@ fn main() {
     let battery_sequences: Vec<_> = banks
         .iter()
         .map(|bank| {
-            let mut biggest_sequence: VecDeque<char> = VecDeque::new();
-
-            let batteries: Vec<char> = bank.chars().collect();
-
-            for &battery in batteries.iter().rev() {
-                let sequence_so_far: String = biggest_sequence.iter().collect();
-                println!("Sequence so far {}", sequence_so_far);
-                println!("Considering {}", battery);
-                if biggest_sequence.len() == num_batteries_to_combine {
-                    println!("Sequence is full, gonna evict");
-                    // Time to evict something!
-                    let mut i = 0;
-                    let mut smallest_seen = ('9', 0);
-                    while i < biggest_sequence.len() - 1 {
-                        let joltage = biggest_sequence[i];
-                        println!("Joltage {}", joltage);
-                        if joltage < smallest_seen.0 {
-                            smallest_seen = (joltage, i);
-                            println!("Smallest seen {} at {}", smallest_seen.0, smallest_seen.1);
-                        }
-                        i += 1;
-                    }
-                    if battery < biggest_sequence[0] {
-                        // Sequence is only going to get smaller if we move the first number in the
-                        // sequence for the sake of a smaller joltage;
-                        continue;
-                    }
-                    if battery > smallest_seen.0 {
-                        println!(
-                            "Removing joltage {} from index {}",
-                            smallest_seen.0, smallest_seen.1
-                        );
-                        biggest_sequence.remove(smallest_seen.1);
-                        biggest_sequence.push_front(battery);
-                    }
-                } else {
-                    // Plenty of room. Fill it up
-                    biggest_sequence.push_front(battery);
-                    println!("Adding {} to the sequence", battery);
-                    continue;
-                }
-                // If the sequence is filled up, whenever we find a new biggest, we can just go
-                //
-                // We know that the current battery we're evaluating is the furthest left we've
-                // seen, so we can look into the sequence and remove the smallest thing from it,
-                // then push the current battery to the front
-                //
-                // 818181911112111
-            }
+            let biggest_sequence = biggest_sequence_for_bank(bank, num_batteries_to_combine);
             return biggest_sequence;
         })
         .collect();
@@ -83,4 +35,94 @@ fn main() {
         .sum();
 
     println!("Output joltage: {}", output_joltage);
+}
+
+fn biggest_sequence_for_bank(bank: &str, num_batteries_to_combine: u32) -> VecDeque<char> {
+    let mut biggest_sequence: VecDeque<char> = VecDeque::new();
+
+    let batteries: Vec<char> = bank.chars().collect();
+
+    for &battery in batteries.iter().rev() {
+        let sequence_so_far: String = biggest_sequence.iter().collect();
+        println!("Sequence so far {}", sequence_so_far);
+        println!("Considering {}", battery);
+        if biggest_sequence.len() == num_batteries_to_combine as usize {
+            println!("Sequence is full, gonna evict");
+            // Time to evict something!
+            let mut i = 0;
+            let mut smallest_seen = ('9', 0);
+            while i < biggest_sequence.len() - 1 {
+                let joltage = biggest_sequence[i];
+                println!("Joltage {}", joltage);
+                if joltage < smallest_seen.0 {
+                    smallest_seen = (joltage, i);
+                    println!("Smallest seen {} at {}", smallest_seen.0, smallest_seen.1);
+                }
+                i += 1;
+            }
+            if battery < biggest_sequence[0] {
+                // Sequence is only going to get smaller if we move the first number in the
+                // sequence for the sake of a smaller joltage;
+                continue;
+            }
+            if battery > smallest_seen.0 {
+                println!(
+                    "Removing joltage {} from index {}",
+                    smallest_seen.0, smallest_seen.1
+                );
+                biggest_sequence.remove(smallest_seen.1);
+                biggest_sequence.push_front(battery);
+            }
+        } else {
+            // Plenty of room. Fill it up
+            biggest_sequence.push_front(battery);
+            println!("Adding {} to the sequence", battery);
+            continue;
+        }
+        // If the sequence is filled up, whenever we find a new biggest, we can just go
+        //
+        // We know that the current battery we're evaluating is the furthest left we've
+        // seen, so we can look into the sequence and remove the smallest thing from it,
+        // then push the current battery to the front
+        //
+        // 818181911112111
+    }
+    return biggest_sequence;
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::VecDeque;
+
+    use crate::biggest_sequence_for_bank;
+
+    #[test]
+    fn it_finds_biggest_sequence_in_between_start_and_end() {
+        let bank = "234234234234278";
+        let sequence = biggest_sequence_for_bank(bank, 12);
+
+        let expected: VecDeque<char> = "434234234278".chars().collect();
+
+        assert_eq!(sequence, expected);
+    }
+
+    #[test]
+    fn it_finds_biggest_sequence_at_start() {
+        let bank = "811111111111119";
+        let sequence = biggest_sequence_for_bank(bank, 12);
+
+        let expected: VecDeque<char> = "811111111119".chars().collect();
+
+        assert_eq!(sequence, expected);
+    }
+
+    #[test]
+    fn it_finds_biggest_sequence_at_end() {
+        let bank = "111191111111119";
+        let sequence = biggest_sequence_for_bank(bank, 12);
+
+        let expected: VecDeque<char> = "191111111119".chars().collect();
+
+        assert_eq!(sequence, expected);
+    }
 }
